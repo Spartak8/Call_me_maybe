823913b3b3278a5f6a1e4e3e25a79a0f628a6793
Spartak8 <77225767+Spartak8@users.noreply.github.com>
2026-08-19 10:44:45 +0400

    parse

--- src/__main__.py
+++ src/__main__.py
@@ -1,5 +1,27 @@
+import argparse
+from pathlib import Path
+
+
 def main() -> None:
-    print("call_me_maybe works")
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--functions_definition",
+        type=Path,
+        default=Path("data/input/functions_definition.json")
+    )
+    parser.add_argument(
+        "--input",
+        type=Path,
+        default=Path("data/input/function_calling_tests.json")
+    )
+    parser.add_argument(
+        "--output",
+        type=Path,
+        default=Path("data/output/function_calling_results.json")
+    )
+    args = parser.parse_args()
+
+    print(args.functions_definition, args.input, args.output)
 
 
 if __name__ == "__main__":
